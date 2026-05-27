c4522a7ec78d4c76298d118468b2582ded32146e
carole tierney <caroleatierney@gmail.com>
2026-05-27 14:49:04 -0400

    fix server log message

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI);
 // ******* ERROR/SUCCESS ********
 // ******************************
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on("connected", () => console.log("mongo connected successfully!"));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 // ******************************
